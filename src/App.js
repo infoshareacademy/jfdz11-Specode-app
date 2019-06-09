686b6ef4bfdc9716cdfe2bb66ee44904e4457ad0
Marcin Jarowski <jarowski.marcin@yahoo.co.uk>
2019-06-09 20:23:13 +0200

    delete default App.js content

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import Button from "@material-ui/core/Button";
 import DayAndCaloriesNav from "./components/DayAndCaloriesNav";
 
 function App() {
   return (
     <div className="App">
       <DayAndCaloriesNav />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button variant="contained" color="primary">
-          Sniadanie
-        </Button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
