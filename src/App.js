2d7add7392934e83ad0b82db62e92b1193086db7
Marcin Jarowski <jarowski.marcin@yahoo.co.uk>
2019-06-09 20:59:36 +0200

    add material icons and todo list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import DayAndCaloriesNav from "./components/DayAndCaloriesNav";
+import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
 
 function App() {
   return (
     <div className="App">
       <DayAndCaloriesNav />
+      <KeyboardArrowLeft fontSize="large" color="primary" />
     </div>
   );
 }
