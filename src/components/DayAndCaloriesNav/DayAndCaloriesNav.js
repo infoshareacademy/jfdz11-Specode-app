9509f4af47a899cc26a28e389d587301617efb67
Marcin Jarowski <jarowski.marcin@yahoo.co.uk>
2019-06-10 17:06:20 +0200

    add moment.js and console.log next and previous day on arrowClick; add one css media query

--- src/components/DayAndCaloriesNav/DayAndCaloriesNav.js
+++ src/components/DayAndCaloriesNav/DayAndCaloriesNav.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./DayAndCaloriesNav.module.css";
 import { KeyboardArrowLeft, KeyboardArrowRight } from "@material-ui/icons";
+import moment from "moment";
 
 const {
   mainContainer,
@@ -8,21 +9,55 @@ const {
   arrowRight,
   selectedDay,
   caloriesBox,
-  dayAndCaloriesBox
+  dateAndCaloriesBox
 } = styles;
 
-function DayAndCaloriesNav() {
+function DayAndCaloriesNav(props) {
+  const [todayFullDate, setNewDate] = useState(moment().format("L"));
+  const [dayName, setNewDay] = useState(moment().format("dddd"));
+
+  let thisDay = moment();
+  let thisDayForChanging = thisDay.clone();
+
+  function addDay() {
+    thisDayForChanging = thisDayForChanging.add(1, "days");
+    let formatToDayName = thisDayForChanging.format("dddd");
+    let formatToDate = thisDayForChanging.format("L");
+    return { formatToDayName, formatToDate };
+  }
+  function subtractDay() {
+    thisDayForChanging = thisDayForChanging.subtract(1, "days");
+    let formatToDayName = thisDayForChanging.format("dddd");
+    let formatToDate = thisDayForChanging.format("L");
+    return { formatToDayName, formatToDate };
+  }
+
   return (
     <div className={mainContainer}>
       <span className={arrowLeft}>
-        <KeyboardArrowLeft fontSize="large" color="inherit" />
+        <KeyboardArrowLeft
+          fontSize="large"
+          color="inherit"
+          onClick={() => {
+            console.log(subtractDay());
+          }}
+        />
       </span>
-      <span className={dayAndCaloriesBox}>
-        <span className={selectedDay}>np. 13 Luty 2019</span>
+      <span className={dateAndCaloriesBox}>
+        <span className={selectedDay}>
+          <span>{dayName}</span>
+          <span>{todayFullDate}</span>
+        </span>
         <span className={caloriesBox}>302 Kcal</span>
       </span>
       <span className={arrowRight}>
-        <KeyboardArrowRight fontSize="large" color="inherit" />
+        <KeyboardArrowRight
+          fontSize="large"
+          color="inherit"
+          onClick={() => {
+            console.log(addDay());
+          }}
+        />
       </span>
     </div>
   );
