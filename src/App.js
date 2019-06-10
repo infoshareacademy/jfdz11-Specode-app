4855d80cb68dcfcd12a704c1736816a73c70c976
Marcin Jarowski <jarowski.marcin@yahoo.co.uk>
2019-06-10 14:14:43 +0200

    Added basic layout for daySwitcher

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "./App.css";
 import DayAndCaloriesNav from "./components/DayAndCaloriesNav";
-import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
 
 function App() {
   return (
     <div className="App">
       <DayAndCaloriesNav />
-      <KeyboardArrowLeft fontSize="large" color="primary" />
     </div>
   );
 }
--- src/components/DayAndCaloriesNav/DayAndCaloriesNav.js
+++ src/components/DayAndCaloriesNav/DayAndCaloriesNav.js
@@ -1,12 +1,29 @@
 import React from "react";
 import styles from "./DayAndCaloriesNav.module.css";
+import { KeyboardArrowLeft, KeyboardArrowRight } from "@material-ui/icons";
 
-const { mainContainer } = styles;
+const {
+  mainContainer,
+  arrowLeft,
+  arrowRight,
+  selectedDay,
+  caloriesBox,
+  dayAndCaloriesBox
+} = styles;
 
 function DayAndCaloriesNav() {
   return (
     <div className={mainContainer}>
-      <span>Siema</span>
+      <span className={arrowLeft}>
+        <KeyboardArrowLeft fontSize="large" color="primary" />
+      </span>
+      <span className={dayAndCaloriesBox}>
+        <span className={selectedDay}>np. 13 Luty 2019</span>
+        <span className={caloriesBox}>302 Kcal</span>
+      </span>
+      <span className={arrowRight}>
+        <KeyboardArrowRight fontSize="large" color="primary" />
+      </span>
     </div>
   );
 }
