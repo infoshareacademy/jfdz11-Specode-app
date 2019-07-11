372dc8ab7e703e61a8d01eddb5d3775f9709f9e8
Paulina <aniskiewicz.paulina@gmail.com>
2019-07-11 08:41:36 +0200

    LocalStorage added to PieChart

--- src/components/PieChart/PieChart.js
+++ src/components/PieChart/PieChart.js
@@ -9,11 +9,21 @@ import {
 } from "@devexpress/dx-react-chart-material-ui";
 import { Animation } from "@devexpress/dx-react-chart";
 
-const data = [
+const data2 = [
   { name: "Jajecznica i pieczywo", kcal: 390, type: "śniadanie" },
   { name: "Wrap z jajkiem i bekonem", kcal: 421, type: "obiad" },
   { name: "Placki owsiane z cukinia", kcal: 327, type: "kolacja" }
 ];
+
+let localArray = JSON.parse(localStorage.getItem("mealsList"));
+let data = localArray.map(obj => {
+  return {
+    type: obj.type,
+    kcal: obj.calories
+  };
+});
+
+console.log(data);
 
 export default class Demo extends React.PureComponent {
   constructor(props) {
@@ -30,7 +40,7 @@ export default class Demo extends React.PureComponent {
     return (
       <Paper>
         <Chart data={chartData}>
-          <PieSeries valueField="kcal" argumentField="name" />
+          <PieSeries valueField="kcal" argumentField="type" />
           <Title text="Daily calories" />
           <Animation />
         </Chart>
