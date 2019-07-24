e77db6b6e0ef8ca59efda6261dc73190090f7f7c
Marcin Jarowski <jarowski.marcin@yahoo.co.uk>
2019-07-24 19:54:49 +0200

    changed size of calendar

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import db from "./firebase";
+import "./firebase";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
