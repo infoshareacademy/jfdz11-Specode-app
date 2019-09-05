820448c9d75ca7693df3dc603049ae6e3a5982a0
Marcin Jarowski <jarowski.marcin@yahoo.co.uk>
2019-09-05 17:06:35 +0200

    remove props, move logic to context

--- src/components/BigCalendar/BigCalendar.js
+++ src/components/BigCalendar/BigCalendar.js
@@ -1,23 +1,23 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import BigCalendar from "react-big-calendar";
 import moment from "moment";
 import { Modal } from "@material-ui/core";
 import { DayView } from "../../scenes";
-import { useState } from "react";
+import { MealsContext } from "../../contexts/mealsContext";
+import { DateContext } from "../../contexts/dateContext";
 
 const localizer = BigCalendar.momentLocalizer(moment);
-// console.log(localizer);
-// var theBigDay = new Date("july 7, 2019");
-// console.log(theBigDay);
-//ustawic state i przekazac do modala ayb tam guzik go zamykal
+
 const MyCalendar = props => {
+  const {
+    meals: { userScheduledMealsArray }
+  } = useContext(MealsContext);
+  const { changeDate, date } = useContext(DateContext);
   const [isModalOpen, setModalOpenState] = useState(false);
 
   const prepareScheduledMealsForCalendar = () => {
-    const { scheduledMealsArray } = props;
-
-    const events = scheduledMealsArray.map(scheduledMeal => {
+    const events = userScheduledMealsArray.map(scheduledMeal => {
       return {
         title: scheduledMeal.type,
         start: scheduledMeal.date,
@@ -33,11 +33,11 @@ const MyCalendar = props => {
     <div>
       <BigCalendar
         onSelectEvent={date => {
-          props.setDate(moment(date.start));
+          changeDate(moment(date.start));
           setModalOpenState(true);
         }}
         onDrillDown={date => {
-          props.setDate(moment(date));
+          changeDate(moment(date));
           setModalOpenState(true);
         }}
         localizer={localizer}
@@ -49,16 +49,7 @@ const MyCalendar = props => {
         }}
       />
       <Modal open={isModalOpen}>
-        <DayView
-          mealsArray={props.mealsArray}
-          updateMealId={props.updateMealId}
-          newMealId={props.newMealId}
-          addMealToSchedule={props.addMealToSchedule}
-          addToMealsArray={props.addToMealsArray}
-          setModalOpenState={setModalOpenState}
-          setDate={props.setDate}
-          dateProps={props.dateProps}
-        />
+        <DayView />
       </Modal>
     </div>
   );
